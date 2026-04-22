1d1090e2fec8e6b2bf9a48dc0983050034de383c
Absolutezero24 <aditiaersa@gmail.com>
2026-04-22 15:19:15 +0700

    fix data ke nfs

--- backend/internal/app/app.go
+++ backend/internal/app/app.go
@@ -64,7 +64,6 @@ func Run() {
 			seeders.SeedPermissions(server.DB)
 			seeders.SeedMasterDataUser(server.DB)
 			seeders.SeedMasterDataAsset(server.DB)
-			seeders.SeedDMS(server.DB)
 			log.Println("Database initialization complete.")
 		}
 
